8d26de9242c6b4eb5f553af53d6946a0c428f16d
吕亚伟 <2215836348@qq.com>
2021-07-14 20:12:03 +0800

    7.14

---
+++ MyTest/src/com/hp/datecollect/Calendar01.java
@@ -0,0 +1,53 @@
+package com.hp.datecollect;
+
+import java.util.Calendar;
+
+/*
+    日历类: 可以做 日期的计算。
+    使用场景，心目中: 预约功能 日期计算功能
+    常用的APP  美柚  疫苗预约 入职日期计算 旅游预订
+    对象如果不能被(无参) new: 1)无参构造私有化了 可能是单例模式
+                          2) 该类可能是个 接口/抽象类 必须 new 它的 实现类(子类)
+     */
+public class Calendar01 {
+        public static void main(String[] args) {
+            // 创建对象 不仅仅只有new ，还有 类的实例方法
+            //  calendar 功能上包含了 date的大部分功能  是date的升级版
+            Calendar calendar = Calendar.getInstance();
+            System.out.println("calendar = " + calendar);
+            // 日历可以进行计算  比如 增加 add
+            // 增加年份  增加十年  场景:买保险 纪念日
+            //calendar.add(Calendar.YEAR,10);
+            //System.out.println("calendar = " + calendar.getTime());
+            //增加月份          增加一月
+            //  calendar.add(Calendar.MARCH, 1);
+            //增加天数         增加 10天
+            //  calendar.add(Calendar.DATE,10);
+            //减去 1天
+            //  calendar.add(Calendar.DATE,-1);
+            // 比如 设置当周的周五日期
+            //  calendar.set(Calendar.DAY_OF_WEEK,Calendar.FRIDAY);
+            System.out.println("calendar.getTime() = " + calendar.getTime());
+            //还能 计算每个月份的最大值                         求月底的时间
+            int days = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+            System.out.println("days = " + days);  //31 天
+            //查询今年 过了多少天
+            int i = calendar.get(Calendar.DAY_OF_YEAR);
+            System.out.println("i = " + i);
+            // 查询今天是周几  国外的日历 从 周日开始
+            int i1 = calendar.get(Calendar.DAY_OF_WEEK)-1;
+            System.out.println("今天是周" + i1);
+            // 今天是 周三
+            String[] week = {"周日","周一","周二","周三","周四","周五","周六"};
+            int i2 = calendar.get(Calendar.DAY_OF_WEEK)-1;
+            System.out.println("今天是" + week[i2]);
+            //  获取 月份 要+1
+            int i3 = calendar.get(Calendar.MONTH)+1;
+            System.out.println("现在是" + i3+"月");
+            // 日历 set...
+            calendar.set(Calendar.YEAR,2008);
+            System.out.println("calendar = " + calendar);
+            calendar.set(Calendar.MONTH,Calendar.MAY); // 月份从0开始
+            System.out.println("calendar = " + calendar.getTime());
+        }
+}
---
+++ MyTest/src/com/hp/datecollect/DateDemo01.java
@@ -0,0 +1,43 @@
+package com.hp.datecollect;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateDemo01 {
+    /*
+        日期类的层是 有一个long值  学名UTC时间
+        日期类 调用 util下的包
+    1970 年 1月1日 00:00:00 开始 计算机时间元年
+    这个 日期类 功能有限, 如需日期计算,需要使用 日历类解决！！
+ */
+    public static void main(String[] args) {
+        // 定义一个日期
+        Date date = new Date();
+        System.out.println("当前日期是 " + date); // date = Wed Jul 14 09:46:28 GMT+08:00 2021
+        // toString()被改写了
+
+        long time = date.getTime(); //获得 真实的的时间，long值
+        System.out.println("time = " + time);
+        //计算十天后的long值
+        long a =  (1000*60*60*24*10L);
+        date.setTime(time+a);
+        System.out.println("十天后的日期 " + date);
+
+        //修改 日期显示格式  yyyy-MM-dd HH:mm:ss.  日期转成字符串
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        String pattern1 = "yyyy/MM/dd HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern1);
+        String ChinaTime = sdf.format(date);
+        System.out.println("ChinaTime = " + ChinaTime);
+        // 字符串 转 日期
+        String datestr = "2021-8-15 23:23:240";
+        try {
+            Date parse = sdf.parse(datestr);
+            System.out.println("parse = " + parse);
+        } catch (ParseException e) {
+            System.out.println("格式错误 请用心输入");
+            e.printStackTrace();
+        }
+    }
+}
